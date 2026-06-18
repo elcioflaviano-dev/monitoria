436bfbf30b6d30e8cef424894d90aad2ce364d78
elcioflaviano-dev <elcio.flaviano@gmail.com>
2026-06-18 12:00:43 -0300

    Update Painel_Rotativo.py

--- pages/Painel_Rotativo.py
+++ pages/Painel_Rotativo.py
@@ -415,6 +415,3 @@ elif st.session_state.idx == 2:
         st.session_state.novo_ciclo = False
         
     st.components.v1.html(st.session_state.script_audio_atual, height=0)
-
-    time.sleep(1)
-    st.rerun()
