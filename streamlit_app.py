76600e282ba2588a15d5eb2922cf790ffe62d070
elcioflaviano-dev <elcio.flaviano@gmail.com>
2026-09-15 07:29:17 -0300

    Refactor streamlit app for improved data handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import os
 import requests
 import io
 
-st.set_page_config(page_title="Monitoria", page_icon="🎯", layout="wide", initial_sidebar_state="collapsed")
+st.set_page_config(page_title="Monitoria - TV", page_icon="🎯", layout="wide", initial_sidebar_state="collapsed")
 
 st.markdown("""
     <style>
